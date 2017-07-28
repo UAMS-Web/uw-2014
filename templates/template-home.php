2fda9ee54c8a06447344ce7c24a93bfc997c0d94
todd-uams <todd@uams.edu>
2017-07-28 11:14:35 -0500

    Added mobile sidebar menu to other template pages

--- templates/template-home.php
+++ templates/template-home.php
@@ -133,6 +133,33 @@ endif;
 	      }
 	  ?>
 
+  	    <div id="mobile-sidebar">
+
+			<button id="mobile-sidebar-menu" class="visible-xs" aria-hidden="true" tabindex="1">
+
+		    	<div aria-hidden="true" id="ham" class="i-org-tree"></div>
+				<div id="mobile-sidebar-title" class="page_item">
+
+			<?php
+			        //limitation of the characters
+			        echo text_cut();
+					function text_cut($text, $length = 27, $dots = true) {
+					$text =get_the_title();
+					$text = trim(preg_replace('#[\s\n\r\t]{2,}#', ' ', $text));
+					$text_temp = $text;
+					   while (substr($text, $length, 1) != " ")
+					    { $length--;
+					  	if ($length > strlen($text)) { break; } }
+					    $text = substr($text, 0, $length);
+					    return $text . ( ( $dots == true && $text != '' && strlen($text_temp) > $length ) ? '...' : '');
+					   }
+			?>
+
+				</div>
+			</button>
+			<div id="mobile-sidebar-links" aria-hidden="true">  <?php uams_sidebar_menu(); ?></div>
+		</div>
+
       <div id='main_content' class="uams-body-copy" tabindex="-1">
 
         <?php
