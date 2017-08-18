b2c6e7af5da5502caba8feb6471f6a1d7aa0c292
todd-uams <todd@uams.edu>
2017-08-18 12:02:40 -0500

    Changed sub-nav icon to reflect standard hamburger

--- templates/template-big-hero.php
+++ templates/template-big-hero.php
@@ -59,25 +59,33 @@
 
 				<button id="mobile-sidebar-menu" class="visible-xs" aria-hidden="true" tabindex="1">
 
-			    	<div aria-hidden="true" id="ham" class="i-org-tree"></div>
+			    	<div aria-hidden="true" id="ham">
+					    <span></span>
+						<span></span>
+						<span></span>
+						<span></span>
+				    </div>
 					<div id="mobile-sidebar-title" class="page_item">
 
-				<?php
-				        //limitation of the characters
-				        echo text_cut();
-						function text_cut($text, $length = 27, $dots = true) {
-						$text =get_the_title();
-						$text = trim(preg_replace('#[\s\n\r\t]{2,}#', ' ', $text));
-						$text_temp = $text;
-						   while (substr($text, $length, 1) != " ")
-						    { $length--;
-						  	if ($length > strlen($text)) { break; } }
-						    $text = substr($text, 0, $length);
-						    return $text . ( ( $dots == true && $text != '' && strlen($text_temp) > $length ) ? '...' : '');
-						   }
-				?>
+						<?php
+					        //limitation of the characters
+					        echo text_cut();
+							function text_cut($text, $length = 27, $dots = true) {
+							$text =get_the_title();
+							$text = trim(preg_replace('#[\s\n\r\t]{2,}#', ' ', $text));
+							$text_temp = $text;
+							   while (substr($text, $length, 1) != " ") {
+									$length--;
+								  	if ($length > strlen($text)) {
+									  	break;
+									}
+								}
+							    $text = substr($text, 0, $length);
+							    return $text . ( ( $dots == true && $text != '' && strlen($text_temp) > $length ) ? '...' : '');
+							}
+						?>
 
-					</div>
+				  	</div>
 				</button>
 				<div id="mobile-sidebar-links" aria-hidden="true">  <?php uams_sidebar_menu(); ?></div>
 			</div>
