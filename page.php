1f1012879d5b316e608e0969d7a5062dce5f9c2e
todd-uams <todd@uams.edu>
2017-09-28 06:02:57 -0500

    Removed commented mobile menu

--- page.php
+++ page.php
@@ -5,16 +5,11 @@
 
 <?php get_template_part( 'header', 'image' ); ?>
 
-<!--<div class="col-md-12 mobile-menu"> <?php get_template_part( 'menu', 'mobile' ); ?> </div>-->
 <div class="container uams-body">
 
   <div class="row">
 
     <div class="col-md-<?php echo ((!isset($sidebar[0]) || $sidebar[0]!="on") ? "8" : "12" ) ?> uams-content" role='main'>
-
-      <?php //uams_page_title(); ?>
-
-      <?php //get_template_part( 'menu', 'mobile' ); ?>
 
       <?php
 	      if((!isset($breadcrumbs[0]) || $breadcrumbs[0]!="on")) {
