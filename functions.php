f08020b16939bf009f1981b2bf8fef50ae77bcdf
todd-uams <todd@uams.edu>
2018-01-09 13:12:31 -0600

    Add Font Awesome v4 shim

--- functions.php
+++ functions.php
@@ -138,6 +138,7 @@ function add_allowed_origins($origins) {
 add_action( 'wp_enqueue_scripts', 'custom_load_font_awesome' );
 function custom_load_font_awesome() {
     wp_enqueue_script( 'font-awesome-all', get_bloginfo('template_directory') . '/js/fontawesome-all.min.js', array(), null, true );
+    wp_enqueue_script( 'font-awesome-v4', get_bloginfo('template_directory') . '/js/fa-v4-shims.min.js', array(), null, true );
     //wp_enqueue_script( 'font-awesome-light', get_bloginfo('template_directory') . '/js/fa-light.min.js', array(), null );
     //wp_enqueue_script( 'font-awesome-brands', get_bloginfo('template_directory') . '/js/fa-brands.min.js', array(), null );
     wp_enqueue_script( 'font-awesome', get_bloginfo('template_directory') . '/js/fontawesome.min.js', array(), null );
@@ -153,7 +154,7 @@ add_filter( 'script_loader_tag', 'add_defer_attribute', 10, 2 );
  * @return   Filtered HTML script tag.
  */
 function add_defer_attribute( $tag, $handle ) {
-    if (( 'font-awesome-all' === $handle ) || ( 'font-awesome-light' === $handle ) || ( 'font-awesome-brands' === $handle )) {
+    if (( 'font-awesome-all' === $handle ) || ( 'font-awesome-light' === $handle ) || ( 'font-awesome-brands' === $handle ) || ( 'font-awesome-v4' === $handle )) {
     	return str_replace( ' src', ' defer src', $tag );
 	} else {
 		return $tag;
