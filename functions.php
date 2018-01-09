aec059cc97bcb15ee972b35c447c6a6203e02512
todd-uams <todd@uams.edu>
2018-01-09 13:36:19 -0600

    FA Tweak

--- functions.php
+++ functions.php
@@ -137,11 +137,11 @@ function add_allowed_origins($origins) {
 // Enqueue Font Awesome.
 add_action( 'wp_enqueue_scripts', 'custom_load_font_awesome' );
 function custom_load_font_awesome() {
-    wp_enqueue_script( 'font-awesome-all', get_bloginfo('template_directory') . '/js/fontawesome-all.min.js', array(), null, false );
-    wp_enqueue_script( 'font-awesome-v4', get_bloginfo('template_directory') . '/js/fa-v4-shims.js', array(), null, false );
     //wp_enqueue_script( 'font-awesome-light', get_bloginfo('template_directory') . '/js/fa-light.min.js', array(), null );
     //wp_enqueue_script( 'font-awesome-brands', get_bloginfo('template_directory') . '/js/fa-brands.min.js', array(), null );
     wp_enqueue_script( 'font-awesome', get_bloginfo('template_directory') . '/js/fontawesome.min.js', array(), null );
+    wp_enqueue_script( 'font-awesome-all', get_bloginfo('template_directory') . '/js/fontawesome-all.min.js', array(), null, false );
+    wp_enqueue_script( 'font-awesome-v4', get_bloginfo('template_directory') . '/js/fa-v4-shims.js', array(), null, false );
 }
 
 add_filter( 'script_loader_tag', 'add_defer_attribute', 10, 2 );
