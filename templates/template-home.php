3620c72e814f816bf2c68e05423001c0af241168
todd-uams <todd@uams.edu>
2018-03-21 13:06:53 -0500

    Home template mobile nav fix

--- templates/template-home.php
+++ templates/template-home.php
@@ -137,31 +137,36 @@ endif;
 
 			<button id="mobile-sidebar-menu" class="visible-xs" aria-hidden="true" tabindex="1">
 
-		    	<div aria-hidden="true" id="ham" class="i-org-tree"></div>
+		    	<div aria-hidden="true" id="ham">
+				    <span></span>
+					<span></span>
+					<span></span>
+					<span></span>
+			    </div>
 				<div id="mobile-sidebar-title" class="page_item">
 
-			<?php
-		        //limitation of the characters
-		        $text = get_the_title();
-		        echo text_cut($text, 27, true);
-				function text_cut($text, $length, $dots) {
-				//$text =get_the_title();
-				$text = trim(preg_replace('#[\s\n\r\t]{2,}#', ' ', $text));
-				$text_temp = $text;
-				   while (substr($text, $length, 1) != " ") {
-						$length--;
-					  	if ($length > strlen($text)) {
-						  	break;
+					<?php
+				        //limitation of the characters
+				        $text = get_the_title();
+				        echo text_cut($text, 27, true);
+						function text_cut($text, $length, $dots) {
+						//$text =get_the_title();
+						$text = trim(preg_replace('#[\s\n\r\t]{2,}#', ' ', $text));
+						$text_temp = $text;
+						   while (substr($text, $length, 1) != " ") {
+								$length--;
+							  	if ($length > strlen($text)) {
+								  	break;
+								}
+							}
+						    $text = substr($text, 0, $length);
+						    return $text . ( ( $dots == true && $text != '' && strlen($text_temp) > $length ) ? '...' : '');
 						}
-					}
-				    $text = substr($text, 0, $length);
-				    return $text . ( ( $dots == true && $text != '' && strlen($text_temp) > $length ) ? '...' : '');
-				}
-			?>
+					?>
 
-				</div>
+			  	</div>
 			</button>
-			<div id="mobile-sidebar-links" aria-hidden="true">  <?php uams_mobile_menu(); ?></div>
+			<div id="mobile-sidebar-links" aria-hidden="true">  <?php uams_sidebar_menu(); ?></div>
 		</div>
 
       <div id='main_content' class="uams-body-copy" tabindex="-1">
