4b47f2dacbe6589babaebe10eac191105d2794f3
todd-uams <todd@uams.edu>
2018-12-10 11:26:28 -0600

    Maps update

--- widgets/class.uams-campus-map.php
+++ widgets/class.uams-campus-map.php
@@ -7,7 +7,7 @@
 class UAMS_Campus_Map extends WP_Widget
 {
 
-  const URL = '//maps.uams.edu/maps/fullscreen/1/?marker=';
+  const URL = '//maps.local/full-screen/?marker=';
 
   function __construct()
   {
@@ -29,7 +29,7 @@ class UAMS_Campus_Map extends WP_Widget
        $content .= "<h3 class=\"widget-title\"> $before_title $title $after_title </h3>";
 
     $content .= '<div class="uams-campus-map-widget">
-                  <iframe width="100%" height="365" src="'.self::URL.$buildingCode.'" style="border:0" allow="geolocation"></iframe>
+                  <iframe width="100%" height="365" src="'.self::URL.$buildingCode.'" style="border:0" allow="geolocation" allowfullscreen mozallowfullscreen="true" webkitallowfullscreen="true" scrolling="no"></iframe>
                   <a href="https://maps.uams.edu/maps/fullscreen/4/?marker='.$buildingCode.'" target="_blank">View larger</a>
                 </div>';
 
